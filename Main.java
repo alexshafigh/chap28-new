6aa8e07d030271c56fd0f0375e8aa1fed31a632e
ali.azimi <saazimi@sadad.co.ir>
2018-07-04 11:15:18 +0430

    upload prs

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 	// write your code here
+//        System.out.println("salam");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/world","root" , "123456789");
+        //Statement
+//        Statement stmnt = connection.createStatement();
+//        ResultSet rst = stmnt.executeQuery("select * from city where CountryCode = 'USA'");
+
+        //Prepared Statement
+
+//        PreparedStatement pstmnt = connection.prepareStatement("select * from city where CountryCode = ?");
+//        pstmnt.setString(1,"IRN");
+//        ResultSet rst = pstmnt.executeQuery();
+//        rst.first();
+//        System.out.printf(" The Name Of Cities in %s Are \n" , rst.getString("CountryCode") );
+
+//        while (rst.next()){
+//            System.out.println(rst.getString("Name"));
+//        }
     }
 }
